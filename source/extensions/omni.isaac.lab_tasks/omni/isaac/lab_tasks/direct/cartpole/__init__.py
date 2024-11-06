9224bcbdd6c8019a5311adecbdcfafa2d0b0fa86
Berkay Gürkan <33008533+berkaygurkan@users.noreply.github.com>
2024-11-07 02:29:45 +0300

    Example environment entry

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cartpole/__init__.py
@@ -12,7 +12,7 @@ import gymnasium as gym
 from . import agents
 from .cartpole_camera_env import CartpoleCameraEnv, CartpoleDepthCameraEnvCfg, CartpoleRGBCameraEnvCfg
 from .cartpole_env import CartpoleEnv, CartpoleEnvCfg
-
+from .cartpole_env_bg import CartpoleEnvTEST, CartpoleEnvCfgBG
 ##
 # Register Gym environments.
 ##
@@ -23,6 +23,19 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": CartpoleEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CartpolePPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Cartpole-Direct-v1",
+    entry_point="omni.isaac.lab_tasks.direct.cartpole:CartpoleEnvTEST",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": CartpoleEnvCfgBG,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CartpolePPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
